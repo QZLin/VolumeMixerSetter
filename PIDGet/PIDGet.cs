7ffdf5520fd3e1379bed2ca6e98f48360a798721
QZLin <qzlin01@163.com>
2022-07-04 21:18:17 +0800

    init commit

---
+++ PIDGet/PIDGet.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using CommandLine;
+using CommandLine.Text;
+
+namespace PIDGet
+{
+    public class PIDGet
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+        public class Options
+        {
+
+            [Value(0, MetaName = "ImageName", Required = false)]
+            public string? ImageName { get; set; }
+
+            [Option("process_name_duplicate_action")]
+            public NameDuplicateAction NameDuplicateOption { get; set; }
+            [Option("process_name_duplicate_err_action")]
+            public NameDuplicateErrorAction NameDuplicateErrorOption { get; set; }
+
+            [Option('m', "matchcase", Required = false, Default = false)]
+            public bool MatchCase { get; set; }
+
+            [Option('h', "help", Required = false)]
+            public string? help_target { get; set; }
+
+        }
+
+        [Flags]
+        public enum NameDuplicateAction
+        {
+            SELECT_FIRST,
+            SELECT_LAST,
+            SELECT_ALL,
+            SELECT_NONE
+        }
+        [Flags]
+        public enum NameDuplicateErrorAction
+        {
+            IGNORE,
+            WARN,
+            ERROR
+        }
+
+        static void Main(string[] args)
+        {
+            var result = new Parser(with => with.HelpWriter = null).ParseArguments<Options>(args);
+
+            result
+            .WithParsed(o =>
+            {
+                if (o.help_target != null)
+                    Help(o.help_target);
+                else if (o.ImageName != null)
+                    Console.WriteLine(Get(o));
+                else
+                    DisplayHelp(result, new List<Error>() { });
+            })
+            .WithNotParsed(errs => DisplayHelp(result, errs));
+        }
+
+        public static void Help(string cmd)
+        {
+            Console.WriteLine(cmd);
+        }
+        static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+        }
+
+        public static int Get(Options o)
+        {
+            string? errors;
+            var matched_list = new List<int>();
+            var all_process = Process.GetProcesses();
+            int result = -1;
+            foreach (var process in all_process)
+            {
+                if (o.MatchCase ? process.ProcessName == o.ImageName :
+                    process.ProcessName.ToLower() == o.ImageName!.ToLower())
+                {
+                    if (o.NameDuplicateOption == NameDuplicateAction.SELECT_FIRST)
+                    {
+                        result = process.Id;
+                        break;
+                    }
+                    else
+                        matched_list.Add(process.Id);
+                }
+            }
+            if (o.NameDuplicateOption == NameDuplicateAction.SELECT_FIRST) { }
+            else if (o.NameDuplicateOption == NameDuplicateAction.SELECT_LAST)
+                result = matched_list[^1];
+            else if (o.NameDuplicateOption == NameDuplicateAction.SELECT_ALL)
+            {
+                Console.WriteLine(string.Join("\n", matched_list));
+                //return ;
+            }
+            else if (o.NameDuplicateOption == NameDuplicateAction.SELECT_NONE)
+                return -1;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using CommandLine;
+using System;
+using VolumeMixerSetter;
+using static PIDGet.PIDGet;
+
+static class Program
+{
+    //[DllImport("user32.dll")]
+    //public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+    //[DllImport("user32.dll", SetLastError = true)]
+    //public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+    public class BaseOptions
+    {
+        [Value(0, MetaName = "PID", Required = false, HelpText = "PID of Application")]
+        public string? Pid { get; set; }
+
+        [Option('m', "mute", Default = false, HelpText = "Specify mute mode")]
+        public bool Mute { get; set; }
+
+        [Option('v', "volume", Default = false, HelpText = "Specify get mode")]
+        public bool Volume { get; set; }
+
+        [Option('n', "process_name", Required = false, HelpText = "Use process name instead of pid")]
+        public string? ProcessName { get; set; }
+
+        [Option('z', "process_name_duplicate_action")]
+        public NameDuplicateAction NameDuplicateOption { get; set; }
+        [Option('y', "process_name_duplicate_err_action")]
+        public NameDuplicateErrorAction NameDuplicateErrorOption { get; set; }
+
+        [Option("verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+    }
+    [Verb("get")]
+    public class Get : BaseOptions
+    {
+
+    }
+
+    [Verb("set")]
+    public class Set : BaseOptions
+    {
+        [Value(1, MetaName = "value", Required = true, HelpText = "[0-100] for volume, 0=true and 1=false for mute")]
+        public int Value { get; set; }
+    }
+
+    static int Main(string[] args)
+    {
+        int pid = -1;
+        var exit_code = 0;
+        var result = Parser.Default.ParseArguments<Get, Set>(args).WithParsed<BaseOptions>(o =>
+        {
+            if (o.ProcessName == null)
+            {
+                exit_code = 1;
+                pid = Convert.ToInt32(o.Pid!);
+                exit_code = 0;
+            }
+            else
+            {
+                var pid_option = new Options
+                {
+                    ImageName = o.ProcessName,
+                    NameDuplicateOption = o.NameDuplicateOption,
+                    NameDuplicateErrorOption = o.NameDuplicateErrorOption
+                };
+                pid = PIDGet.PIDGet.Get(pid_option);
+                if (pid == -1)
+                    exit_code = 1;
+            }
+            if (!o.Mute & !o.Volume)
+            {
+                Console.Error.WriteLine("must select an action [mute/volume]");
+                exit_code = 1;
+            }
+        })
+        .WithParsed<Get>(o =>
+        {
+
+            if (o.Mute)
+            {
+                var mute = VolumeMixer.GetApplicationMute(pid);
+                Func<bool?, int>? f = (mute is null)
+                ? s => { Console.Error.WriteLine(s); return 1; }
+                : s => { Console.WriteLine(s); return 0; };
+                exit_code = f(mute);
+            }
+            else if (o.Volume)
+            {
+                var volume = VolumeMixer.GetApplicationVolume(pid);
+                Func<float?, int>? f = (volume is null)
+                ? s => { Console.Error.WriteLine(s); return 1; }
+                : s => { Console.WriteLine(s); return 0; };
+                exit_code = f(volume);
+            }
+        })
+        .WithParsed<Set>(o =>
+        {
+            if (o.Mute)
+            {
+                VolumeMixer.SetApplicationMute(pid, o.Value == 1);
+            }
+            else if (o.Volume)
+            {
+                VolumeMixer.SetApplicationVolume(pid, o.Value);
+            }
+        });
+        return exit_code;
+    }
+}
